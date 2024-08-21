55aba7843c352ec3fd8a1c3d130ab1cb08ea09ea
Manahil2026 <manahil2004@gmail.com>
2024-08-21 11:31:58 -0400

    add components folder and CustomAppBar file

---
+++ rmp-ai-assistant/app/components/CustomAppBar.js
@@ -0,0 +1,77 @@
+'use client'
+import { useState } from 'react'
+import { Box, 
+    Button,
+    Stack,
+    TextField,
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    List,ListItem,ListItemButton,ListItemIcon,ListItemText,} from '@mui/material'
+
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/Inbox'; 
+import MailIcon from '@mui/icons-material/Mail'; 
+
+
+import Image from "next/image";
+
+
+const CustomAppBar = ({ defaultTitle }) => {
+
+const [open, setOpen] = useState(false);
+const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+};
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+          <List>
+            {['ChatBot'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      );
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Rate My Professor AI Support
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+          </Drawer>
+        </Box>
+      );
+
+
+
+
+}
+
+export default CustomAppBar;
