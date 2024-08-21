878dfe27498bb54fc0e57247194162908f23b4f9
Manahil2026 <manahil2004@gmail.com>
2024-08-21 15:11:08 -0400

    delete unecessary import statements

--- rmp-ai-assistant/app/components/CustomAppBar.js
+++ rmp-ai-assistant/app/components/CustomAppBar.js
@@ -11,8 +11,6 @@ import { Box,
     Drawer,
     List,ListItem,ListItemButton,ListItemIcon,ListItemText,} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
-import InboxIcon from '@mui/icons-material/Inbox'; 
-import MailIcon from '@mui/icons-material/Mail'; 
 import { usePathname, useRouter } from "next/navigation";
 
 
