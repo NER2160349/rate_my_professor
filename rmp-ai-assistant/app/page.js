40cf3c2842a36e38d6bda115349e50501d7087f1
Manahil2026 <manahil2004@gmail.com>
2024-08-21 11:33:03 -0400

    import CustomAppBar

--- rmp-ai-assistant/app/page.js
+++ rmp-ai-assistant/app/page.js
@@ -1,11 +1,39 @@
 'use client'
-import { Box, Button, Stack, TextField } from '@mui/material'
 import { useState } from 'react'
+import { Box, 
+    Button,
+    Stack,
+    TextField,
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    List,ListItem,ListItemButton,ListItemIcon,ListItemText,} from '@mui/material'
+
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/Inbox'; 
+import MailIcon from '@mui/icons-material/Mail'; 
+import CustomAppBar from "./components/CustomAppBar";
+
+
 import Image from "next/image";
 
 
 export default function Home() {
-  
 
-    }
+    return(
+        <CustomAppBar />
+    )
+
+
+
+
+
+
+
+
+
+
+}
     
