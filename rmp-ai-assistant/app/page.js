a979ba403ca6ab76f4515d57064c00f062e328a0
Syeda Begum <begum.syeda2003@gmail.com>
2024-08-21 17:42:57 -0400

    merge page.js

--- rmp-ai-assistant/app/page.js
+++ rmp-ai-assistant/app/page.js
@@ -1,11 +1,40 @@
 'use client'
-import { Box, Button, Stack, TextField } from '@mui/material'
 import { useState } from 'react'
+import { Box, 
+    Button,
+    Stack,
+    TextField,
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Drawer,
+    List,ListItem,ListItemButton,ListItemIcon,ListItemText,} from '@mui/material'
+import {ThemeProvider} from '@mui/material/styles';
+
+import CustomAppBar from "./components/CustomAppBar";
+import CustomTheme from "./components/Theme";
+
+
+
 import Image from "next/image";
 
 
 export default function Home() {
-  
+
+    return(
+        <ThemeProvider theme={CustomTheme}>
+        <CustomAppBar />
+        </ThemeProvider>
+    )
+
+
+
+
+
+
+
+
+
 
     }
-
