4e8cd43fe4058c5945b2ba1791dbbf48dce2f11e
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-08-23 16:48:57 -0400

    pushing files

--- rmp-ai-assistant/app/api/scrape/route.js
+++ rmp-ai-assistant/app/api/scrape/route.js
@@ -5,8 +5,18 @@ import path from 'path';
 export async function POST(request) {
   const { professorUrl } = await request.json();
 
-  // Define the Python script path
-  const scriptPath = path.join(process.cwd(), 'rmp-ai-assistant-python', 'scrape_professor_data.py');
+  // Determine which script to run based on the URL
+  let scriptName;
+  if (professorUrl.includes('ratemyprofessors.com/professor')) {
+    scriptName = 'scrape_professor_data.py';
+  } else if (professorUrl.includes('ratemyprofessors.com/school')) {
+    scriptName = 'scrape_school_data.py';
+  } else {
+    return new Response(JSON.stringify({ success: false, error: 'Invalid URL provided' }), { status: 400 });
+  }
+
+  // Define the Python script path based on the determined script
+  const scriptPath = path.join(process.cwd(), 'rmp-ai-assistant-python', scriptName);
 
   return new Promise((resolve, reject) => {
     exec(`python3 ${scriptPath} ${professorUrl}`, (error, stdout, stderr) => {
