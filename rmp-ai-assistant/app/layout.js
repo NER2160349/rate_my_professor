a308d19e80247f7dc750fb95e644e9dfe66e0f29
Manahil2026 <manahil2004@gmail.com>
2024-08-25 20:02:50 -0400

    update title

--- rmp-ai-assistant/app/layout.js
+++ rmp-ai-assistant/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rate My Professor AI Support",
+  title: "RateMyProfessorAI",
   description: "Rate My Professor AI chatbot",
 };
 
